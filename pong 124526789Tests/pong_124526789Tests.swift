40d73e878f27a8c31c345382e9b3a51d7d729bad
Benjamin <bcyze@jh-mba158-07.local>
2018-04-24 14:30:30 -0500

    Initial Commit

---
+++ pong 124526789Tests/pong_124526789Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pong_124526789Tests.swift
+//  pong 124526789Tests
+//
+//  Created by Benjamin on 4/24/18.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+import XCTest
+@testable import pong_124526789
+
+class pong_124526789Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
