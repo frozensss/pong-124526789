a7768caa6e7e9816863b90f54fecc05cf25eab88
Jason Rosu <jrosu1315@jh-mba158-22.local>
2018-04-26 14:49:41 -0500

    ball and paddle code paddle position

--- pong 124526789/GameScene.swift
+++ pong 124526789/GameScene.swift
@@ -10,13 +10,58 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var topPaddle = SKSpriteNode()
+    var bottomPaddle = SKSpriteNode()
+    var rightPaddle = SKSpriteNode()
+    var leftPaddle = SKSpriteNode()
+    
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        
+        topPaddle = self.childNode(withName: "topPaddle") as! SKSpriteNode
+        bottomPaddle = self.childNode(withName: "bottomPaddle") as! SKSpriteNode
+        rightPaddle = self.childNode(withName: "rightPaddle") as! SKSpriteNode
+        leftPaddle = self.childNode(withName: "leftPaddle") as! SKSpriteNode
+        
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
     self.physicsBody = border
     }
+    
     func didBegin(_ contact: SKPhysicsContact) {
-     print(contact)
+     print(contact.bodyA.node?.name)
 }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        bottomPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+        leftPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        rightPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        
+              topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+              bottomPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+              leftPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+              rightPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
